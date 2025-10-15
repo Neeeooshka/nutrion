4afbe6acf99966f7815fa572f08cfa6655daf8f4
neooshka <neo@msi.ru>
2025-10-15 04:05:01 +0300

    fixup! fix errors

--- telegram/app/handlers/message.py
+++ telegram/app/handlers/message.py
@@ -1,5 +1,6 @@
 from aiogram import Router, types, F, Bot
 from aiogram.fsm.context import FSMContext
+from aiogram.enums import ChatAction
 from backend.llm_memory import ask_llm
 from backend.llm_history import add_to_memory
 from backend.llm_profile import get_profile
@@ -27,13 +28,13 @@ async def handle_message(msg: types.Message, state: FSMContext):
     user_input = msg.text
     try:
         # Фраза размышления
-        await msg.bot.send_chat_action(chat_id=chat_id, action=types.ChatAction.TYPING)
+        await msg.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
         thinking_text = get_random_phrase()
         await asyncio.sleep(1)
         await msg.answer(thinking_text)
         
         # Запрашиваем модель (LLM)
-        await msg.bot.send_chat_action(chat_id=chat_id, action=types.ChatAction.TYPING)
+        await msg.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
         answer = await ask_llm(chat_id, user_id, user_input)
         await add_to_memory(chat_id, user_id, user_input, answer)
         
